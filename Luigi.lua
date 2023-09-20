a9031ad981e5ffad8f96836d3717c927bc30f558
Ícaro Rivero <60977696+irgamarra@users.noreply.github.com>
2023-09-20 22:30:00 +0200

    Create Luigi.lua

---
+++ Luigi.lua
@@ -0,0 +1,19 @@
+
+function chooseDiscards()
+    discardCard()
+end
+
+function discardCard()
+end
+
+function draw()
+end
+
+function getHand()
+end
+
+function loseMoney()
+end
+
+function collectMoney()
+end
